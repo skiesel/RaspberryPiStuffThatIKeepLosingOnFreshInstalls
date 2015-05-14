8ff65d590dee3fa6baa3ade8a551deacf4c84ac7
Scott Kiesel <skiesel@cs.unh.edu>
2015-05-14 19:53:41 -0400

    Create screenOff.sh

---
+++ screenOff.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ! -d  /sys/class/gpio/export ];
+then
+  sudo sh -c "echo 508 > /sys/class/gpio/export"
+  sudo sh -c "echo 'out' > /sys/class/gpio/gpio508/direction"
+fi
+
+sudo sh -c "echo '0' > /sys/class/gpio/gpio508/value"
